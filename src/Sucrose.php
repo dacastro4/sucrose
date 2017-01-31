ee419f198dcbb256ae2aecc6ed094ee614a8f3e3
Matt Watson <matt@metromediaworks.com>
2017-01-31 18:44:03 -0500

    Deleted field and setter for boolean field type in Sugar

--- src/Sucrose.php
+++ src/Sucrose.php
@@ -9,7 +9,16 @@ class Sucrose extends Model {
     const CREATED_AT = 'date_entered';
 
     const UPDATED_AT = 'date_modified';
-    
+
+    const DELETED_AT = 'deleted';
+
     public $incrementing = false;
+
+    public function setDeletedAttribute()
+    {
+        if(!is_null($this->attribute['deleted'])) {
+          $this->attribute['deleted'] = 1;
+        }
+    }
 
 }
