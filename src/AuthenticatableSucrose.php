8df8b377311b7970f71286f2fa23d07f262ec91b
Matt Watson <matt@metromediaworks.com>
2017-01-31 10:50:59 -0500

    Updated name of Authenticatable class

--- src/AuthenticatableSucrose.php
+++ src/AuthenticatableSucrose.php
@@ -4,12 +4,12 @@ namespace AgileElement;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Sucrose extends Authenticatable {
+class AuthenticatableSucrose extends Authenticatable {
 
     const CREATED_AT = 'date_entered';
 
     const UPDATED_AT = 'date_modified';
-    
+
     public $incrementing = false;
 
 }
