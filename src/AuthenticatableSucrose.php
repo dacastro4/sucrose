9d6cd09b0e9966e30f3ff8851934a9a5878bfb41
Matt Watson <matt@metromediaworks.com>
2017-01-31 02:50:09 -0500

    Spacing matters

--- src/AuthenticatableSucrose.php
+++ src/AuthenticatableSucrose.php
@@ -7,7 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Sucrose extends Authenticatable {
 
     const CREATED_AT = 'date_entered';
+
     const UPDATED_AT = 'date_modified';
+    
     public $incrementing = false;
 
 }
--- src/Sucrose.php
+++ src/Sucrose.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Sucrose extends Model {
 
     const CREATED_AT = 'date_entered';
+
     const UPDATED_AT = 'date_modified';
+    
     public $incrementing = false;
 
 }
